cc18ab575bbd4b9ab3ae2a25ca4f51b520f86252
Marshall <marspatt@ttu.edu>
2022-03-21 20:39:47 -0500

    Scanner code

--- src/Scanner.java
+++ src/Scanner.java
@@ -1,2 +1,54 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class Scanner {
+
+    private File file;
+    private int state;
+    private final List tokens;
+
+    public Scanner(File file) throws IOException {
+        this.file = file;
+        tokens = new ArrayList<String>();
+        parseFile();
+    }
+
+    public void parseFile() throws IOException {
+        // while not EOF run automata
+        BufferedReader br = new BufferedReader(new FileReader(file));
+
+        // Declaring a string variable
+        String st;
+        // Condition holds true till
+        // there is character in a string
+        while ((st = br.readLine()) != null)
+
+            // Print the string
+            System.out.println(st);
+    }
+
+    public void automata() {
+        switch (this.state) {
+            case 1:
+                break;
+        }
+    }
+
+    public File getFile() {
+        return file;
+    }
+
+    public void setFile(File file) {
+        this.file = file;
+    }
+
+    public int getState() {
+        return state;
+    }
+
+    public void setState(int state) {
+        this.state = state;
+    }
 }
