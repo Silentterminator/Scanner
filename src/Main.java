450b34a0aa72dafb90d6f0283dc5c9bdb4ffd127
Marshall <marspatt@ttu.edu>
2022-03-21 19:41:54 -0500

    Some file logic

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
+import java.io.File;
+
 public class Main {
 
     public static void main(String[] args) {
+        File file = new File("input.txt");
+
+        if (file.isFile()) {
+            // Logic here to scan file
+        }
 
     }
 }
