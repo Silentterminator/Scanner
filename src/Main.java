ca62f76d1c083449e2e16335715f73db594d63ed
Marshall <marspatt@ttu.edu>
2022-03-21 20:39:36 -0500

    Exception handling and scanner constructor

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,15 @@
 import java.io.File;
+import java.io.IOException;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         File file = new File("input.txt");
 
         if (file.isFile()) {
+            Scanner scan = new Scanner(file);
             // Logic here to scan file
+
         }
 
     }
