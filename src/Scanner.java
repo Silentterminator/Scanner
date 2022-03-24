375983b42d73cf8543ef00474d441d62469f45ec
Megan <griegermegan@gmail.com>
2022-03-24 12:56:59 -0500

    Now able to check what is contained in the list/array

--- src/Scanner.java
+++ src/Scanner.java
@@ -1,6 +1,10 @@
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
 
 
 public class Scanner {
@@ -8,6 +12,7 @@ public class Scanner {
     private File file;
     private int state;
     private final List tokens;
+    //public int numbers{0,1,2,3,4,5,6,7,8,9};
 
     public Scanner(File file) throws IOException {
         this.file = file;
@@ -20,18 +25,35 @@ public class Scanner {
         BufferedReader br = new BufferedReader(new FileReader(file));
 
         // Declaring a string variable
-        String st;
+        String line = br.readLine();
+        ArrayList<String> st = new ArrayList<String>();
+
         // Condition holds true till
         // there is character in a string
-        while ((st = br.readLine()) != null)
-
+        while (line != null)
+        {
             // Print the string
-            System.out.println(st);
+                //System.out.println(st);
+            st.add(line);
+            line = br.readLine();
+        }
+
+        for(int x = 0; x<st.size();x++)
+        {
+            if(st.get(x).contains("read")||st.get(x).contains("Read"))
+                tokens.add("read");
+            if(st.get(x).contains("write")||st.get(x).contains("Write"))
+                tokens.add("write");
+        }
+        System.out.println(tokens);
+
+        br.close();
     }
 
     public void automata() {
         switch (this.state) {
             case 1:
+
                 break;
         }
     }
@@ -51,4 +73,5 @@ public class Scanner {
     public void setState(int state) {
         this.state = state;
     }
+
 }
