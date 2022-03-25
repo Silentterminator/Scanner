cb1b155b9c866853d12cce8173b92ce291d16be7
Marshall <marspatt@ttu.edu>
2022-03-24 20:16:25 -0500

    tokens list, assigned variables instead of independent access

--- src/Test.java
+++ src/Test.java
@@ -6,6 +6,8 @@ public class Test {
 
     private final File file;
     private final List<Character> characterArray = new ArrayList<>();
+    private final List<String> tokens = new ArrayList<>();
+
     private final char EMPTY_CHAR_CHARACTER = '\u0000';
     private final char NEW_LINE_CHARACTER = '\n';
 
@@ -26,24 +28,44 @@ public class Test {
         boolean inComment = false;                          // Variable to let us know if we're in a comment or not
         char commentCharacter = EMPTY_CHAR_CHARACTER;       // Reference to if we're in a // comment or /* comment
         for (int i = 0; i < characterArray.size(); i++) {
+
+            char currentChar = characterArray.get(i);
+            char nextChar = characterArray.get(i+1);
+
             // if we have /* or //, set inComment to true, that way we don't parse more values until end of line or end of comment
-            if (characterArray.get(i) == '/' && (characterArray.get(i+1) == '*' || characterArray.get(i+1) == '/')) {
+            if (currentChar == '/' && (nextChar == '*' || nextChar == '/')) {
                 inComment = true;
                 commentCharacter = characterArray.get(i+1);
             }
             if (inComment) {
                 // check for closing comment - we're in a // comment if the first condition is met
-                if (commentCharacter == '/' && characterArray.get(i) == NEW_LINE_CHARACTER) {
+                if (commentCharacter == '/' && currentChar == NEW_LINE_CHARACTER) {
                     inComment = false;
                     commentCharacter = EMPTY_CHAR_CHARACTER;
-                } else if (characterArray.get(i) == '*' && characterArray.get(i+1) == '/') {
+                } else if (currentChar == '*' && nextChar == '/') {
                     inComment = false;
                     commentCharacter = EMPTY_CHAR_CHARACTER;
                     i++;
                 }
             } else {
-                // TODO OTHER CHARACTER CHECK HERE
+                // search for (
+                if (currentChar == '(') {
+                    tokens.add("lparen");
+                    while ()
+                }
+                // Search for +
+                // search for -
+                // search for *
+                // search for /
+                // search for )
+                // do the fucking math (parse tree?)
+                // add to tokens
 
+                // 5+1 ----> number plus number
+                // assign a variable??? := WALRUS OPERATOR print("assign")
+                // search for ., if followed by digit^, print("number")
+                // regular digit 99999999, print("number") BUT after finds digits, look for period, signifies a decimal, PRINT("NUMBER")
+                // letter,digit = token - letter = token
             }
         }
     }
