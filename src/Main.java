0a8b724cc5e6e5283b54f8030c99c5f3988f48f2
Marshall <marspatt@ttu.edu>
2022-03-24 19:30:23 -0500

    Swapped to Test scanner

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,13 @@ public class Main {
     public static void main(String[] args) throws IOException {
         File file = new File("input.txt");
 
-        if (file.isFile()) {
-            Scanner scan = new Scanner(file);
+//        if (file.isFile()) {
+//            Scanner scan = new Scanner(file);
             // Logic here to scan file
 
-        }
+//        }
 
+        Test test = new Test(file);
+        test.parse();
     }
 }
