8174c78810a6faeef58d78047cf0af6d3acb834f
Marshall <marspatt@ttu.edu>
2022-03-24 20:16:31 -0500

    input2

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        File file = new File("input.txt");
+        File file = new File("input2.txt");
 
 //        if (file.isFile()) {
 //            Scanner scan = new Scanner(file);
