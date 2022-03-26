e2b224e3262788c8be39c026bde7b58c73f5ebb9
Marshall <marspatt@ttu.edu>
2022-03-26 14:13:14 -0500

    Re-added logic to main method to check if file attempting to be scanned exists

--- src/Main.java
+++ src/Main.java
@@ -6,13 +6,10 @@ public class Main {
     public static void main(String[] args) throws IOException {
         File file = new File("input2.txt");
 
-//        if (file.isFile()) {
-//            Scanner scan = new Scanner(file);
-            // Logic here to scan file
-
-//        }
-
-        Scanner test = new Scanner(file);
-        test.parse();
+        // If file exists, we can scan it
+        if (file.isFile()) {
+            Scanner scan = new Scanner(file);
+            scan.parse();
+        }
     }
 }
