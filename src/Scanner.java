a16fb5389945d14d3abedfa81b806d3e1d26209b
Marshall <marspatt@ttu.edu>
2022-03-26 14:07:40 -0500

    Code and comment cleanup

--- src/Scanner.java
+++ src/Scanner.java
@@ -3,34 +3,30 @@ import java.util.ArrayList;
 import java.util.List;
 import static java.lang.System.exit;
 
-public class Scanner
-{
+public class Scanner {
 
     private final File file;
     private final List<Character> characterArray = new ArrayList<>();
     private final List<String> tokens = new ArrayList<>();
     private final List<Character> errorChar = new ArrayList<>();
 
-    //How to find the comments
+    // How to find the comments
     private final char EMPTY_CHAR_CHARACTER = '\u0000';
     private final char NEW_LINE_CHARACTER = '\n';
-    //Helps find digit.digit
+    // Helps find digit.digit
     private final char PERIOD = '.';
 
-    public Scanner(File file)
-    {
+    public Scanner(File file) {
         this.file = file;
     }
 
-    public void parse() throws IOException
-    {
-        //reading file
+    public void parse() throws IOException {
+        // Reads text from file
         BufferedReader br = new BufferedReader(new FileReader(file));
 
         int c;
         // Read characters in and store them in our character array
-        while ((c = br.read()) != -1)
-        {
+        while ((c = br.read()) != -1) {
             char ch = (char) c;
             characterArray.add(ch);
         }
@@ -40,103 +36,80 @@ public class Scanner
         errorChar.add('`'); errorChar.add('~');
 
         boolean inComment = false;                          // Variable to let us know if we're in a comment or not
-        char commentCharacter = EMPTY_CHAR_CHARACTER;       // Reference to if we're in a // comment or /* comment
-        for (int i = 0; i < characterArray.size()-1; i++)
-        {
-            char currentChar = characterArray.get(i);
-            char nextChar = characterArray.get(i+1);        //peek value
+        char commentCharacter = EMPTY_CHAR_CHARACTER;       // Holds what type of comment we are in, denoted by second character, '/' or '*'
 
-            if(currentChar == 'r' && nextChar == 'e' && characterArray.get(i+2) == 'a' && characterArray.get(i+3) == 'd' )
+        for (int i = 0; i < characterArray.size()-1; i++) {
+            char currentChar = characterArray.get(i);
+            char nextChar = characterArray.get(i+1);        // Peek value
+
+            // Check for "read"
+            if(currentChar == 'r' && nextChar == 'e' && characterArray.get(i+2) == 'a' && characterArray.get(i+3) == 'd' ) {
                 tokens.add("read");
-            if(currentChar == 'w' && nextChar == 'r' && characterArray.get(i+2) == 'i' && characterArray.get(i+3) == 't' && characterArray.get(i+4) == 'e')
+            }
+
+            // Check for "write"
+            if(currentChar == 'w' && nextChar == 'r' && characterArray.get(i+2) == 'i' && characterArray.get(i+3) == 't' && characterArray.get(i+4) == 'e') {
                 tokens.add("write");
+            }
+
             // if we have /* or //, set inComment to true, that way we don't parse more values until end of line or end of comment
-            if (currentChar == '/' && (nextChar == '*' || nextChar == '/'))
-            {
+            if (currentChar == '/' && (nextChar == '*' || nextChar == '/')) {
                 inComment = true;
                 commentCharacter = characterArray.get(i+1);
             }
-            if (inComment)
-            {
+            if (inComment) {
                 // check for closing comment - we're in a // comment if the first condition is met
-                if (commentCharacter == '/' && currentChar == NEW_LINE_CHARACTER)
-                {
+                if (commentCharacter == '/' && currentChar == NEW_LINE_CHARACTER) {
                     inComment = false;
                     commentCharacter = EMPTY_CHAR_CHARACTER;
                 }
-                else
-                    if (currentChar == '*' && nextChar == '/')
-                    {
-                        inComment = false;
-                        commentCharacter = EMPTY_CHAR_CHARACTER;
-                        i++;
-                    }
-            }
-            else
-                {
+                // Check for */ to close comment
+                if (currentChar == '*' && nextChar == '/') {
+                    inComment = false;
+                    commentCharacter = EMPTY_CHAR_CHARACTER;
+                    i++;
+                }
+            } else {
                     //Finding error characters within characterArray
-                    for (int j = 0; j<errorChar.size()-1;j++)
-                    {
-                        if (currentChar == (errorChar.get(j)))
-                        {
+                    for (int j = 0; j<errorChar.size()-1;j++) {
+                        if (currentChar == (errorChar.get(j))) {
                             System.out.println("Error");
                             exit(0);                        //Exiting program (found error character)
                         }
                     }
 
-                    if (Character.isDigit(currentChar) || (currentChar == PERIOD && Character.isDigit(nextChar)))
-                    {
-                        while (Character.isDigit(nextChar) || nextChar == PERIOD)
-                        {
+                    if (Character.isDigit(currentChar) || (currentChar == PERIOD && Character.isDigit(nextChar))) {
+                        while (Character.isDigit(nextChar) || nextChar == PERIOD) {
                             i++;
                             nextChar = characterArray.get(i+1);
                         }
                         tokens.add("number");
                     }
-                    if (currentChar == '(')
-                    {
+                    if (currentChar == '(') {
                         tokens.add("lparen");
+                    } else if (currentChar == '*') {
+                        tokens.add("times");
+                    } else if (currentChar == '/') {
+                        tokens.add("div");
+                    } else if (currentChar == '+') {
+                        tokens.add("plus");
+                    } else if (currentChar == '-') {
+                        tokens.add("minus");
+                    } else if (currentChar == ')') {
+                        tokens.add("rparen");
                     }
-                    else
-                        if (currentChar == '*')
-                        {
-                            tokens.add("times");
-                        }
-                        else
-                            if (currentChar == '/')
-                            {
-                                tokens.add("div");
-                            }
-                            else
-                                if (currentChar == '+')
-                                {
-                                    tokens.add("plus");
-                                }
-                                else
-                                    if (currentChar == '-')
-                                    {
-                                        tokens.add("minus");
-                                    }
-                                    else
-                                        if (currentChar == ')')
-                                        {
-                                            tokens.add("rparen");
-                                        }
 
-                    if (currentChar == ':' && nextChar == '=')
-                    {
+                    if (currentChar == ':' && nextChar == '=') {
                         tokens.add("assign");
                     }
-                    if (Character.isAlphabetic(currentChar)&&Character.isAlphabetic(nextChar))
-                    {
-                        while(Character.isAlphabetic(nextChar)&&Character.isAlphabetic(nextChar+1)||Character.isDigit(nextChar+1))
-                        {
+
+                    if (Character.isAlphabetic(currentChar) && Character.isAlphabetic(nextChar)) {
+                        while(Character.isAlphabetic(nextChar) && Character.isAlphabetic(nextChar+1) || Character.isDigit(nextChar+1)) {
                             i++;
                             nextChar = characterArray.get(i+1);
                         }
                         tokens.add("id");
                     }
-
                 }
         }
         System.out.println(tokens);
